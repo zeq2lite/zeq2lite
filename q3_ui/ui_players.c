e23f98eeaff1d3978f283c84fcde184e03b887d1
DavidOmari <DavidOmari@1e7f69a8-e265-0410-9af5-ca8027663dd9>
2009-04-17 11:47:35 +0000

    NEW WIP Desert map.
    git-svn-id: http://zeq2.com/SVN/Source@316 1e7f69a8-e265-0410-9af5-ca8027663dd9

--- q3_ui/ui_players.c
+++ q3_ui/ui_players.c
@@ -979,7 +979,7 @@ void UI_DrawPlayer( float x, float y, float w, float h, playerInfo_t *pi, int ti
 	// add the chat icon
 	//
 	if ( pi->chat ) {
-		UI_PlayerFloatSprite( pi, origin, trap_R_RegisterShaderNoMip( "sprites/balloon3" ) );
+		UI_PlayerFloatSprite( pi, origin, trap_R_RegisterShaderNoMip( "gfx/misc/balloon" ) );
 	}
 
 	//
