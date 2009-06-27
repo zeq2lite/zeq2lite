7789115e85e42dffa0a5727f11a100cd56d771f8
DavidOmari <DavidOmari@1e7f69a8-e265-0410-9af5-ca8027663dd9>
2009-06-27 02:59:16 +0000

    -- Using new team ally icon indicator, instead of the quake 3 one. -- Fixed it so team ally icon doesn't show on your own character on your view.
    git-svn-id: http://zeq2.com/SVN/Source@662 1e7f69a8-e265-0410-9af5-ca8027663dd9

--- cgame/cg_players.c
+++ cgame/cg_players.c
@@ -2136,7 +2136,8 @@ static void CG_PlayerSprites( centity_t *cent ) {
 	team = cgs.clientinfo[ cent->currentState.clientNum ].team;
 	if ( !(cent->currentState.eFlags & EF_DEAD) && 
 		cg.snap->ps.persistant[PERS_TEAM] == team &&
-		cgs.gametype >= GT_TEAM) {
+		cgs.gametype >= GT_TEAM &&
+		cent->currentState.number != cg.snap->ps.clientNum) {
 		if (cg_drawFriend.integer) {
 			CG_PlayerFloatSprite( cent, cgs.media.friendShader );
 		}
